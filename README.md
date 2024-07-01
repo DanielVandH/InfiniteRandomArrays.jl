35dd4fb6209f91aa552fbb5fa1637847a0d2d595
DanielVandH <danj.vandenheuvel@gmail.com>
2024-07-01 14:23:09 +0100

    Implement

--- README.md
+++ README.md
@@ -1,3 +1,53 @@
 # InfiniteRandomArrays
 
 [![Build Status](https://github.com/DanielVandH/InfiniteRandomArrays.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/DanielVandH/InfiniteRandomArrays.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+Julia package for defining infinite random arrays using `InfRandVector` and `InfRandMatrix`.
+
+```julia-repl
+julia> using InfiniteRandomArrays, Random
+
+julia> seq = InfRandVector()
+ℵ₀-element InfRandVector{Float64, Type{Float64}, Xoshiro} with indices 1:∞:
+ 0.45212042137198805
+ 0.9647238362580713
+ 0.6462526785368201
+ 0.6070361635715884
+ 0.10222222882722942
+ ⋮
+
+julia> using Distributions
+
+julia> seq = InfRandVector(Xoshiro(); dist = Normal()) # you can provide your own RNG objects and probability distributions
+ℵ₀-element InfRandVector{Float64, Normal{Float64}, Xoshiro} with indices 1:∞:
+ -0.2504330818050723
+  0.5311482824138374
+ -0.44943761206068344
+ -0.05495674359736308
+ -1.3213053212880623
+  ⋮
+
+julia> mat = InfRandMatrix() # ∞ × ∞ by default
+ℵ₀×ℵ₀ InfRandMatrix{Float64, InfRandVector{Float64, Type{Float64}, Xoshiro}, Infinities.Infinity, Infinities.Infinity} with indices OneToInf()×OneToInf():
+ 0.984449   0.203976   0.898197  0.84345   0.745756  0.368674  0.351961  0.461869  …  
+ 0.585687   0.610724   0.267032  0.495627  0.697247  0.873579  0.44229   0.301071
+ 0.534054   0.557934   0.478424  0.843203  0.723205  0.339773  0.731446  0.18291
+ 0.0206029  0.0462717  0.160456  0.76735   0.869515  0.365665  0.476832  0.565982
+ 0.49287    0.616739   0.637889  0.870164  0.770499  0.616812  0.580221  0.370818
+ ⋮                                                   ⋮                             ⋱
+
+julia> mat = InfRandMatrix(3, ∞) # wide random matrix
+3×ℵ₀ InfRandMatrix{Float64, InfRandVector{Float64, Type{Float64}, Xoshiro}, Int64, Infinities.Infinity} with indices Base.OneTo(3)×OneToInf():
+ 0.900203  0.261395  0.222175  0.429518  0.732901  0.820293  0.972021  0.0544778  …  
+ 0.914717  0.538762  0.46626   0.907753  0.453501  0.307607  0.440711  0.896696
+ 0.96409   0.556647  0.784155  0.699887  0.47419   0.35258   0.788991  0.481014      
+
+julia> mat = InfRandMatrix(Xoshiro(), ∞, 7; dist = Exponential()) # tall random matrix
+ℵ₀×7 InfRandMatrix{Float64, InfRandVector{Float64, Exponential{Float64}, Xoshiro}, Infinities.Infinity, Int64} with indices OneToInf()×Base.OneTo(7):
+ 2.3061    0.381158  0.405392  0.0152963  0.424681  0.880358  1.3035
+ 0.988759  0.238397  1.32133   2.33813    0.626563  1.06349   1.19391
+ 2.00946   2.7429    0.97791   1.68287    0.712554  1.26766   0.0432282
+ 0.260173  0.947835  0.668184  0.405158   1.81257   0.195216  3.52343
+ 1.0597    0.680587  0.616505  0.100519   0.387551  2.80689   0.63101
+ ⋮                                                  ⋮
+```
--- src/InfiniteRandomArrays.jl
+++ src/InfiniteRandomArrays.jl
@@ -1,5 +1,118 @@
 module InfiniteRandomArrays
 
-# Write your package code here.
+import Random: default_rng, seed!, AbstractRNG
+import LazyArrays: AbstractCachedVector, resizedata!, LazyMatrix
+import InfiniteArrays: ℵ₀, ∞, Infinity, InfiniteCardinal
+
+export InfRandVector, InfRandMatrix, ∞
+
+get_antidiagonal_bidx(i, j) = i + j - 1 # what antidiagonal does (i, j) belong to 
+function diagtrav_idx(i, j) # assumes that the parent matrix is ∞ × ∞
+    # maps (i, j) to its linear index when traversing the antidiagonals 
+    band = get_antidiagonal_bidx(i, j)
+    nelm_prev = (band * (band - 1)) ÷ 2 # number of elements in the previous band-1 antidiagonals 
+    return nelm_prev + i
+end
+
+_dist_type(dist) = typeof(dist)
+_dist_type(dist::Type{T}) where {T} = Type{T} # avoid DataType for typeof(Float64), since DataType means rand(seq.rng, seq.dist) is inferred as Any
+function _reset_seed!(rng)
+    new_seed = rand(rng, UInt32)
+    return seed!(rng, new_seed)
+end
+
+"""
+    InfRandVector([rng=default_rng()]; dist=Float64)
+
+Represents a random infinite sequence. The random number generator can be specified 
+by the first argument `rng`, which defaults to `Random.default_rng()`, and the distribution 
+to generate from can be specified using the `dist` keyword argument, which defaults to `Float64`.
+
+!!! note "Mutation of rng"
+
+    When an `InfRandVector` is constructed, the `rng` (or, if 
+    no `rng` is provided, the global `rng`) gets reseeded
+    using `Random.seed!(rng, rand(rng, UInt32))`.
+"""
+mutable struct InfRandVector{T,D,RNG} <: AbstractCachedVector{T}
+    const rng::RNG
+    const dist::D
+    const data::Vector{T}
+    datasize::Int
+end
+
+function InfRandVector(rng::AbstractRNG=default_rng(); dist=Float64)
+    T = typeof(rand(copy(rng), dist))
+    _rng = copy(rng)
+    _reset_seed!(rng)
+    return InfRandVector{T,_dist_type(dist),typeof(_rng)}(_rng, dist, T[], 0)
+end
+
+Base.size(::InfRandVector) = (∞,)
+Base.axes(::InfRandVector) = (1:∞,)
+Base.length(::InfRandVector) = ∞
+
+@inline _single_rand(seq::InfRandVector) = rand(seq.rng, seq.dist)
+function resizedata!(seq::InfRandVector, inds)
+    newlen = maximum(inds)
+    curlen = length(seq.data)
+    newlen > curlen || return seq
+    resize!(seq.data, newlen)
+    # rand!(seq.rng, view(seq.data, curlen+1:newlen), seq.dist)
+    # ^ rand() is not actually sequential.. rand(Random.seed!(123), 1000) ≠ (rng = Random.seed!(123); [rand(rng) for _ in 1:1000])
+    for i in (curlen+1):newlen
+        seq.data[i] = _single_rand(seq)
+    end
+    seq.datasize = newlen
+    return seq
+end
+
+"""
+    InfRandMatrix([rng=default_rng()], m, n; dist=Float64])
+
+Represents a random infinite matrix with `m` rows and `n` columns. At least one of `m` 
+and `n` must be infinite. The random number generator 
+can be specified by the first argument `rng`, which defaults to `Random.default_rng()`.
+The `dist` keyword argument (default `Float64`) can be used to specify the distribution to sample from. 
+
+!!! note "Mutation of rng"
+
+    When an `InfRandMatrix` is constructed, the `rng` (or, if 
+    no `rng` is provided, the global `rng`) gets reseeded
+    using `Random.seed!(rng, rand(rng, UInt32))`.
+"""
+struct InfRandMatrix{T, S <: InfRandVector{T}, M, N} <: LazyMatrix{T} 
+    seq::S 
+    m::M
+    n::N
+    function InfRandMatrix(seq::S, m::M, n::N) where {T, S<:InfRandVector{T}, M, N}
+        !(isinf(m) || isinf(n)) && throw(ArgumentError("The matrix must be infinite along at least one axis."))
+        return new{T, S, M, N}(seq, m, n)
+    end
+end 
+const IntOrInf = Union{InfiniteCardinal{0}, Infinity, Integer}
+InfRandMatrix(; dist = Float64) = InfRandMatrix(default_rng(); dist)
+InfRandMatrix(rng::AbstractRNG; dist = Float64) = InfRandMatrix(rng, ∞, ∞; dist)
+InfRandMatrix(m::IntOrInf, n::IntOrInf; dist=Float64) = InfRandMatrix(default_rng(), m, n; dist)
+InfRandMatrix(rng::AbstractRNG, m::IntOrInf, n::IntOrInf; dist=Float64) = InfRandMatrix(InfRandVector(rng; dist), m, n)
+_is_wide(A::InfRandMatrix) = isfinite(A.m)
+_is_tall(A::InfRandMatrix) = isfinite(A.n)
+function Base.getindex(A::InfRandMatrix, i::Int, j::Int)
+    ((i < 1) || (i > A.m) || (j < 1) || (j > A.n)) && throw(BoundsError(A, (i, j)))
+    lin = _sub2ind(A, i, j)
+    return A.seq[lin]
+end
+
+Base.size(A::InfRandMatrix) = (A.m, A.n)
+
+function _sub2ind(A::InfRandMatrix, i, j)
+    if _is_wide(A)
+        return (j - 1) * A.m + i 
+    elseif _is_tall(A) 
+        return (i - 1) * A.n + j 
+    else 
+        return diagtrav_idx(i, j)
+    end
+end
 
 end
